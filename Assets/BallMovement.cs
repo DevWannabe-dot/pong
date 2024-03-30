f4a85f722d0e5152bc06d9fe834d64ec6e831978
Pedro H. <pedro.26.3.h@gmail.com>
2024-03-30 01:03:59 -0300

    More interesting gameplay

--- Assets/BallMovement.cs
+++ Assets/BallMovement.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class BallMovement : MonoBehaviour
 {
     public float speed = 10.0f;
+    public float multiplier = 1.01f;
 
     void Start()
     {
-        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+        GetComponent<Rigidbody2D>().velocity = (new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f))) * speed;
     }
 
     float hitFactor(Vector2 ballPos, Vector2 plrPos, float size)
@@ -22,13 +23,15 @@ public class BallMovement : MonoBehaviour
         {
             float y = hitFactor(transform.position, col.transform.position, col.collider.bounds.size.y);
             Vector2 direction = new Vector2(1, y).normalized;
-            GetComponent<Rigidbody2D>().velocity = direction * speed;
+            GetComponent<Rigidbody2D>().velocity = direction * speed * multiplier;
+            multiplier *= 1.01f;
         }
         if (col.gameObject.name == "CPU")
         {
             float y = hitFactor(transform.position, col.transform.position, col.collider.bounds.size.y);
             Vector2 direction = new Vector2(-1, y).normalized;
-            GetComponent<Rigidbody2D>().velocity = direction * speed;
+            GetComponent<Rigidbody2D>().velocity = direction * speed * multiplier;
+            multiplier *= 1.01f;
         }
     }
 }
